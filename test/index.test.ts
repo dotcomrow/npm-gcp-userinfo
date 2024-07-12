865f7e3ff64215b778f36c3b707736e19034ea03
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-07-12 15:02:22 -0400

    scope cleanup
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,6 +6,6 @@ const keyFilePath = '/Users/admin/secrets/google.key'; // replace with the path 
 const keyFileContent = fs.readFileSync(keyFilePath, 'utf8');
 
 var accessToken = new GCPAccessToken(keyFileContent);
-var token = await accessToken.getAccessToken('openid profile email https://www.googleapis.com/auth/admin.directory.group.readonly');
+var token = await accessToken.getAccessToken('https://www.googleapis.com/auth/admin.directory.group.readonly');
 
 console.log(await GCPUserInfo.getUserInfo(token.access_token, "113903586019246158170", "suncoast.systems"))
